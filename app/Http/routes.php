0633b7847be26b8be841378d688ee8433620a35c
sabbir ahmed <sabbir.ove@gmail.com>
2017-09-04 12:25:44 +0600

    master page for admin ste up

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,14 +1,7 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
+
 */
 
 Route::get('/', function () {
@@ -18,3 +11,8 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+Route::get('/admin', function(){
+	return view('admin.index');
+});
+
+Route::resource('admin/users','AdminUsersController');
